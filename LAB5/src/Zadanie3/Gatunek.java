1b4d76599e5ef0a3124196cc0453f53dd7f1a705
Matias014 <131909661+Matias014@users.noreply.github.com>
2023-05-04 17:26:54 +0200

    Delete LAB5/src directory

--- LAB5/src/Zadanie3/Gatunek.java
+++
@@ -1,9 +0,0 @@
-package Zadanie3;
-
-public class Gatunek {
-
-    private String nazwa_rodzaju;
-    private String nazwa_gatunkowa;
-    private int liczba_chromosomow_2n;
-    private int podstawowa_liczba_chromosomow_x;
-}
--- LAB5/src/Zadanie3/Zadanie3.java
+++
@@ -1,9 +0,0 @@
-package Zadanie3;
-
-public class Zadanie3 {
-
-    public static void main(String[] args) {
-
-        
-    }
-}
